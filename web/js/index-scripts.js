7f308ce810060f6df7716949d8d1d13358591fc2
TheMFhalls <maiconjunioheredeslopes130696@gmail.com>
2017-05-23 00:21:57 -0300

    20170523

--- web/js/index-scripts.js
+++ web/js/index-scripts.js
@@ -19,6 +19,42 @@ function selectEstados(elemento){
     });
 }
 
+function selectCidades(elemento){
+    var destino = $("select[name='select-cidade']");
+    var idEstado = elemento.val();
+    
+    //Limpar SELECT de cidades        
+        destino.empty();
+        destino.attr("disabled", "disabled"); 
+    //Fim limpar SELECT de cidades          
+    
+    $.ajax({
+        url: window.location.href+
+        "estado/"+idEstado+"/cidade",
+        contentType: 'application/json',
+        type: "GET",
+        dataType: "json"
+    }).done(function(data){       
+        if(data.length){
+            destino.append($('<option>', {
+                text: "-- Selecione sua cidade --"
+            }));
+            
+            $.each(data, function(){
+                var item = $(this)[0];
+                destino.append($('<option>', {
+                    value: item.idCidade,
+                    text: item.nome
+                }));
+            });  
+            
+            destino.removeAttr("disabled");
+        }        
+    }).fail(function(){
+        alert("Erro ao selecionar as Cidades!");
+    });
+}
+
 $(document).ready(function(){
     selectEstados("select[name='select-estado']");
 });
