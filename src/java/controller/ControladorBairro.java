aaf5e62c5138c3c01a5d572790eed8ecda58f2ed
TheMFhalls <maiconjunioheredeslopes130696@gmail.com>
2017-05-31 01:00:20 -0300

    20170531

--- src/java/controller/ControladorBairro.java
+++ src/java/controller/ControladorBairro.java
@@ -4,6 +4,7 @@ import controller.logica.Logica;
 import controller.logica.bairro.AtualizarBairro;
 import controller.logica.bairro.CadastrarBairro;
 import controller.logica.bairro.EncontrarBairro;
+import controller.logica.bairro.EncontrarEscolaBairro;
 import controller.logica.bairro.ExcluirBairro;
 import controller.logica.bairro.ExibirBairro;
 import java.io.IOException;
@@ -17,20 +18,30 @@ public class ControladorBairro extends Controlador {
         throws ServletException, IOException {
          try{
             String[] pathParts = (String[]) request.getAttribute("pathParts");
-
+            
             if(pathParts.length == 4){
                 Logica logica = (Logica) new EncontrarBairro();
 
                 logica.executa(request, response);
 
                 outJson(request, response, "bairro");
+            }else if(pathParts.length > 4){
+                switch(pathParts[4]){
+                    case "escola":
+                        Logica logica = (Logica) new EncontrarEscolaBairro();
+
+                        logica.executa(request, response);
+
+                        outJson(request, response, "escolas");
+                    break;
+                }                
             }else{
                 Logica logica = (Logica) new ExibirBairro();
 
                 logica.executa(request, response);
 
                 outJson(request, response, "bairros");                 
-            }                      
+            }
         }catch(Exception e){
           System.out.println ("Erro: classe ControladorEstado - não foi possível executar a lógica de negócio.");
         }
